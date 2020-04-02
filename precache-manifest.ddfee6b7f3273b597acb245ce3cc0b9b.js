3054d8d8ab9367c003450eac1d4f2047d1089c88
flipmz <zorigmr@gmail.com>
2020-04-03 01:53:01 +0800

    Updates

---
+++ precache-manifest.ddfee6b7f3273b597acb245ce3cc0b9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674a7b4c3034dfe89381c2cdbc48afed",
+    "url": "/app1/index.html"
+  },
+  {
+    "revision": "606c1ce95f9c6afeaf29",
+    "url": "/app1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f2927626204d41e8ff0",
+    "url": "/app1/static/js/2.84de93d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app1/static/js/2.84de93d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606c1ce95f9c6afeaf29",
+    "url": "/app1/static/js/main.e1099d61.chunk.js"
+  },
+  {
+    "revision": "1ec157a54d2e9df2c310",
+    "url": "/app1/static/js/runtime-main.7813a1e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app1/static/media/logo.5d5d9eef.svg"
+  }
+]);
